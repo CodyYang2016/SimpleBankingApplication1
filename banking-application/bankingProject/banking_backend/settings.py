021352e5ee61b7846dd233e6aec08098dac4aa7c
Cody Yang <yang.6403@osu.edu>
2024-03-13 19:25:46 -0400

    Cleanup + settings

--- banking-application/bankingProject/banking_backend/settings.py
+++ banking-application/bankingProject/banking_backend/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'bankingAPI.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '',
+        'USER': 'postgres',
+        'PASSWORD': 'Cody2326',
+        'HOST': 'localhost',
+        'PORT': '5432',
+
     }
 }
 
